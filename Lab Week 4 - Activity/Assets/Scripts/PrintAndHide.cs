cc2fea96074fcd6d2d7e1f83028dde4d614ac0da
TomKat707 <13899543@student.uts.edu.au>
2020-08-19 14:44:03 +1000

    90% completed
    reached the 90% mark

---
+++ Lab Week 4 - Activity/Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    int i = 0;
+
+    public Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        print( gameObject.name + ": " + i);
+    
+        i++;
+    }
+}
